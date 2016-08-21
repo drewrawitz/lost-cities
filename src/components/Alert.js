366c4627db0b1ab18761b787a17f19b20ab19f1b
Drew Rawitz <rawitz@sanbornmediafactory.com>
2016-08-20 20:13:29 -0400

    moving animation styles to its own partial

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -12,7 +12,7 @@ class Alert extends Component {
 
     if(this.props.alert) {
       alertMarkup = (
-        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true} transitionEnterTimeout={300} transitionLeaveTimeout={300} transitionAppearTimeout={300}>
+        <ReactCSSTransitionGroup transitionName="transition--fade" transitionAppear={true} transitionEnterTimeout={300} transitionLeaveTimeout={300} transitionAppearTimeout={300}>
           <div className={alertClasses}>{this.props.alert.message}</div>
         </ReactCSSTransitionGroup>
       )
