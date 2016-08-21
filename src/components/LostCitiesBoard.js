b66216749b14aa797bd468b67c56b6328c120a46
Drew Rawitz <rawitz@sanbornmediafactory.com>
2016-08-21 03:37:02 -0400

    using CSS for the tooltip hover instead of react state

--- src/components/LostCitiesBoard.js
+++ src/components/LostCitiesBoard.js
@@ -8,28 +8,15 @@ class LostCitiesBoard extends Component {
     super(props)
 
     this.discardCard = this.discardCard.bind(this)
-    this.discardMouseOver = this.discardMouseOver.bind(this)
-    this.discardMouseOut = this.discardMouseOut.bind(this)
 
     this.state = {
-      selected: {},
-      hovered: false
+      selected: {}
     }
-  }
-
-  discardMouseOver() {
-    this.setState({ hovered: true })
-  }
-
-  discardMouseOut() {
-    this.setState({ hovered: false })
   }
 
   discardCard() {
     // card has been discarded, we now need to take a card from the board
     this.props.updateAction('take')
-
-    console.log('discarded');
   }
 
   render() {
@@ -43,18 +30,13 @@ class LostCitiesBoard extends Component {
         'lost-cities__discard-wrapper--' + selected.color
       )
 
-      let tooltipClasses = classNames(
-        'lost-cities__tooltip-wrapper',
-        {'is-shown': this.state.hovered}
-      )
-
       discard = (
         <ReactCSSTransitionGroup transitionName="transition--fade" transitionAppear={true} transitionEnterTimeout={300} transitionLeaveTimeout={300} transitionAppearTimeout={300}>
           <div className={discardWrapperClasses}>
-            <div className={tooltipClasses}>
+            <div className="lost-cities__tooltip-wrapper">
               <span className="lost-cities__tooltip">Discard</span>
             </div>
-            <div className="lost-cities__discard" onClick={this.discardCard} onMouseOver={this.discardMouseOver} onMouseOut={this.discardMouseOut}></div>
+            <div className="lost-cities__discard" onClick={this.discardCard}></div>
           </div>
         </ReactCSSTransitionGroup>
       )
