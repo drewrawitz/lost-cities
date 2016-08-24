d6eb44f38c53309145e628418a01283fc2b48567
Drew Rawitz <rawitz@sanbornmediafactory.com>
2016-08-24 12:17:25 -0400

    showing all discard piles regardless of selected card

--- src/components/LostCitiesBoard.js
+++ src/components/LostCitiesBoard.js
@@ -32,47 +32,45 @@ class LostCitiesBoard extends Component {
     let action = this.props.action
     let selected = (this.props.players[turn]) ? this.props.players[turn].selected : null
 
-    if(!_.isEmpty(selected) || action === 'take') {
-      let tooltip = (action === 'take') ? 'Pick up' : 'Discard'
+    let tooltip = (action === 'take') ? 'Pick up' : 'Discard'
 
-      _.each(piles, function(pile) {
-        let discardPileClasses = classNames(
-          'lost-cities__discard-wrapper',
-          (pile) ? "lost-cities__discard-wrapper--" + pile: null
-        )
+    _.each(piles, function(pile) {
+      let discardPileClasses = classNames(
+        'lost-cities__discard-wrapper',
+        (pile) ? "lost-cities__discard-wrapper--" + pile: null
+      )
 
-        highlightMarkup = (
-          <ReactCSSTransitionGroup transitionName="transition--fade" transitionAppear={true} transitionEnterTimeout={300} transitionLeaveTimeout={300} transitionAppearTimeout={300}>
-            <div className="lost-cities__discard-highlight" onClick={(action === 'take') ? self.drawCard.bind(self) : self.discardCard.bind(self, selected.color, selected)}>
-              <div className="lost-cities__tooltip-wrapper">
-                <span className="lost-cities__tooltip">{tooltip}</span>
-              </div>
+      highlightMarkup = (
+        <ReactCSSTransitionGroup transitionName="transition--fade" transitionAppear={true} transitionEnterTimeout={300} transitionLeaveTimeout={300} transitionAppearTimeout={300}>
+          <div className="lost-cities__discard-highlight" onClick={(action === 'take') ? self.drawCard.bind(self) : self.discardCard.bind(self, selected.color, selected)}>
+            <div className="lost-cities__tooltip-wrapper">
+              <span className="lost-cities__tooltip">{tooltip}</span>
             </div>
-          </ReactCSSTransitionGroup>
-        )
-
-        if(pile === selected.color) {
-          pileHighlight = highlightMarkup
-          deckHighlight = null
-        } else if (pile === 'deck' && action === 'take') {
-          deckHighlight = highlightMarkup
-          pileHighlight = null
-        } else {
-          pileHighlight = null
-          deckHighlight = null
-        }
-
-        let markup = (
-          <div className={discardPileClasses} key={`discard-${pile}`}>
-            {pileHighlight}
-            {deckHighlight}
-            <ul className="lost-cities__discard-pile"></ul>
           </div>
-        )
+        </ReactCSSTransitionGroup>
+      )
 
-        markupArray.push(markup)
-      })
-    }
+      if(pile === selected.color) {
+        pileHighlight = highlightMarkup
+        deckHighlight = null
+      } else if (pile === 'deck' && action === 'take') {
+        deckHighlight = highlightMarkup
+        pileHighlight = null
+      } else {
+        pileHighlight = null
+        deckHighlight = null
+      }
+
+      let markup = (
+        <div className={discardPileClasses} key={`discard-${pile}`}>
+          {pileHighlight}
+          {deckHighlight}
+          <ul className="lost-cities__discard-pile"></ul>
+        </div>
+      )
+
+      markupArray.push(markup)
+    })
 
     return (
       <div className="lost-cities__board-wrapper">
