3d2fe51d7f0b3490307a1d40414b8fc54b156c8c
Drew Rawitz <rawitz@sanbornmediafactory.com>
2016-08-28 13:19:46 -0400

    adding firebase

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'; // we need this for react-router
+import {firebaseStateReducer} from 'redux-firebasev3'
 import game from './game'
 import board from './board'
 
 // Combine all our reducers togeher
 const rootReducer = combineReducers({
+  firebase: firebaseStateReducer,
   deck: game.deck,
   players: game.players,
   turn: game.turn,
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router'
+import {reduxReactFirebase} from 'redux-firebasev3'
 import rootReducer from './reducers/index';
 import helpers from './lib/helpers';
 
@@ -18,10 +19,19 @@ const defaultState = {
   discarded: board.discarded
 };
 
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCC8d0LQJb4A1y3r8yv0-dt03NEi09ZHsU",
+  authDomain: "lost-cities-d5b5b.firebaseapp.com",
+  databaseURL: "https://lost-cities-d5b5b.firebaseio.com",
+  storageBucket: "",
+}
+
 const store = createStore(
   rootReducer,
   defaultState,
   compose(
+    reduxReactFirebase(firebaseConfig),
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f,
   )
